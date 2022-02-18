740b377237a5ccf1f523ab4e315f692cbaca2ad9
sunk <824819754@qq.com>
2022-02-18 14:54:05 +0800

    user

---
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/study/ProviderUserApplication.java
@@ -0,0 +1,18 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @Author sunk
+ * @Date 2022/2/18
+ */
+@SpringBootApplication
+public class ProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUserApplication.class, args);
+    }
+}
