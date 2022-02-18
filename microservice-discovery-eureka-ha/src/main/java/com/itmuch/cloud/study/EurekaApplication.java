35db4b6b5184386274af91246a9f1dbd878926f1
sunk <824819754@qq.com>
2022-02-18 18:17:27 +0800

    microservice-discovery-eureka-ha

---
+++ microservice-discovery-eureka-ha/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现
+ * @Author sunk
+ * @Date 2022/2/18
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
