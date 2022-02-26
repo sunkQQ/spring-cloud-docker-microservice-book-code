e3da1f0ae31caaf5a95891d5c64c9b3676bb5969
Administrator <824819754@qq.com>
2022-02-26 17:13:56 +0800

    microservice-consumer-movie-without-eureka

---
+++ microservice-consumer-movie-without-eureka/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,46 @@
+package com.itmuch.cloud.study.user.controller;
+
+import com.itmuch.cloud.study.user.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author sunk
+ * @Date 2022/2/18
+ */
+@RestController
+public class MovieController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
+    @Value("user.userServiceUrl")
+    private String userServiceUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        //return this.restTemplate.getForObject(this.userServiceUrl  + id, User.class);
+        return this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        // 打印当前选择的是哪个节点
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getInstanceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    }
+
+}
