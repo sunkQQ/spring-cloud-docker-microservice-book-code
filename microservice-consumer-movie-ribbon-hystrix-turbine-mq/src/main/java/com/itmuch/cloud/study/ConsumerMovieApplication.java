0570a814da29b3a04f4234bf82fb355457330ed2
Administrator <824819754@qq.com>
2023-02-21 19:10:01 +0800

    microservice-hystrix-turbine-mq microservice-consumer-movie-ribbon-hystrix-turbine-mq

---
+++ microservice-consumer-movie-ribbon-hystrix-turbine-mq/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -0,0 +1,29 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author sunk
+ * @Date 2022/2/18
+ */
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerMovieApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+}
